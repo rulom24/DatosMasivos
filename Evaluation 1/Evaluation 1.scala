1e3efd0403b1f2a694958976202f4b1c8fcf734a
Juan Antonio Aceves Zamora <60381311+juanito96az@users.noreply.github.com>
2021-04-15 12:01:46 -0700

    Update Evaluation 1.scala

--- Evaluation 1/Evaluation 1.scala
+++ Evaluation 1/Evaluation 1.scala
@@ -1,25 +1,36 @@
-//Iniciar sesión en Spark
+//1. Iniciar sesión en Spark.
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 val spark = SparkSession.builder().getOrCreate()
 
-//Cargar Netflix Stock CSV
+//2. Cargar Netflix Stock CSV.
 val NetDF = spark.read.option("header", "true")
             .option("inferSchema", "true")
             csv("C:\\Users\\brise\\Documents\\GitHub\\DatosMasivos\\Evaluation 1\\Netflix_2011_2016.csv")
 
-//Nombres de las columnas
+//3. Cuales son los nombres de las columnas?
 val ColNames: Array[String] = NetDF.columns
 ColNames.foreach(name => println(s"$name"))
 
+//4. Como se llama el esquema? 
 NetDF.printSchema()
 
-//Imprimir las primeras 5 columnas
+//5. Imprimir las primeras 5 columnas.
+NetDF.head(5)
 
-
-//Usa describe()
+//6. Usa describe()
 NetDF.describe()
 
-//Crea un nuevo dataframe con una columna nueva llamada “HV Ratio” que es la
-//relación entre el precio de la columna “High” frente a la columna “Volume” de
-//acciones negociadas por un día. (Hint: Es una operación de columnas).
+//7.  Crea una nueva columna llamada "HV Ratio" que es la relacion entre el precio
+//    de la columna "High" frente a la columna "Volumen" de acciones negociadas por un dia.
+// Se agrego la nueva columna "HV Ratio" la cual tendra el resultado de la division de "High" entre "Volume".
+
+val NetDF2 = df.withColumn("HV Ratio",df("High")/df("Volume"))
+df2.show()
+
+//9. ¿Cual es el significado de la columna Cerrar "Close"?
+// Respuesta: Es el cierre del mes.
+
+//10. ¿Cual es el maximo y minimo de la columna "Volumen"?
+df.select(max("Volume")).show()
+df.select(min("Volume")).show()
