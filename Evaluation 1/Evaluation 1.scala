7da047a2369baa15366e5659290a44d0806f6253
Juan Antonio Aceves Zamora <60381311+juanito96az@users.noreply.github.com>
2021-04-15 18:19:38 -0700

    Update Evaluation 1.scala

--- Evaluation 1/Evaluation 1.scala
+++ Evaluation 1/Evaluation 1.scala
@@ -34,3 +34,35 @@ NetDF2.show()
 //10. What is the maximum and minimum of the column "Volume"?
 NetDF.select(max("Volume")).show()
 NetDF.select(min("Volume")).show()
+
+//11. Con sintaxis Scala/Spark $ conteste los siguientes: 
+
+//a) ¿Cuantos dias fue la columna "Close" inferior a $600?
+df.filter($"Close"<600).count()
+ 
+
+// b) ¿Que porcentaje del tiempo fue la columna "High" mayor que $500?
+(df.filter($"High" > 500).count() * 1.0/ df.count())*100
+
+// c) ¿Cual es la correlacion de Pearson entre Columna "High" y la columna "Volume"?
+df.select(corr("High","Volume")).show()
+
+// Se importa la sintaxis de SPARK para la consulta de datos 
+import spark.implicits._
+
+// d) ¿Cual es el maximo de la columna "High" por año?
+// Se agrego la columna "Year" del dato "Date", en la variable "yeardf"
+val yeardf = df.withColumn("Year",year(df("Date")))
+// Se selecciono a partir de la variable "yeardf" el año y el maximo de "High", a partir de maximos a minimo en los años
+val yearmaxs = yeardf.select($"Year",$"High").groupBy("Year").max()
+// Se dio como resultado el maximo de la columna "High" por año
+val res = yearmaxs.select($"Year",$"max(High)")
+res.orderBy("Year").show()
+
+// e) ¿Cual es el promedio de la columna "Close" para cada mes del calendario?
+// Se agrego la columna "Month" del dato "Date", en la variable "monthdf"
+val monthdf = df.withColumn("Month",month(df("Date")))
+// Se selecciono a partir de la variable "monthdf" el mes y el promedio de "Close"
+val monthavgs = monthdf.select($"Month",$"Close").groupBy("Month").mean()
+// Se selecciono y mostro el promedio de la columna "Close" para cada mes del calendario
+monthavgs.select($"Month",$"avg(Close)").orderBy("Month").show()
